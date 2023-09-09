120db359c48704841c60c83feea3fbd2d793d438
Gionata Massi <gionata.massi@savoiabenincasa.it>
2023-09-09 13:28:52 +0200

    Aggiunta compilazione del curricolo come elenco

--- compila.sh
+++ compila.sh
@@ -1,1 +1,2 @@
+deno run deno run curricolo.ts > ls_curricolo.tex 
 pdflatex curricolo_sa && bibtex8 curricolo_sa && pdflatex curricolo_sa
--- curricolo.ts
+++ curricolo.ts
@@ -11,21 +11,26 @@ export interface Argomento {
   argomento: string;
   arg_breve: string;
   area_tematica:
+    | "Algoritmi e linguaggi di programmazione (AL)"
     | "Architettura dei computer (AC)"
+    | "Basi di dati (BD)"
+    | "Computazione, calcolo numerico e simulazione (CS)"
     | "Elaborazione digitale dei documenti (DE)"
-    | "Algoritmi e linguaggi di programmazione (AL)";
+    | "Reti di computer (RC)"
+    | "Sistemi operativi (SO)"
+    | "Struttura di Internet e servizi (IS)";
   rationale: string;
   strumenti: string;
   risultati: string;
   abilita: string[];
 }
 
-interface Tratto {
+interface Ciclo {
   periodo: "Primo biennio" | "Secondo biennio" | "Quinto anno";
   argomenti: Argomento[];
 }
 
-const context: Tratto[] = [
+const context: Ciclo[] = [
   {
     periodo: "Primo biennio",
     argomenti: [
@@ -82,48 +87,102 @@ const context: Tratto[] = [
         strumenti: `Le attività di apprendimento interattivo fanno ricorso alla manipolazione di carte da gioco, attività estrapolate da \\cite{csu} e giochi interattivi presenti nella sezione "Interactives" di \\cite{csfg}.
         Vengono condivise delle risorse di studio con la descrizione degli algoritmi in pseudo-linguaggio, diagramma delle attività e due linguaggi di programmazione.
         L'insegnante esegue passo passo gli algoritmi alla lavagna illustrando le variazioni di stato.`,
-        abilita: ["", "", "", "", ""],
+        abilita: [
+          "Saper formalizzare un problema di ricerca",
+          "Simulare l'esecuzione dell'algoritmo di ricerca lineare",
+          "Simulare l'esecuzione dell'algoritmo di ricerca binaria",
+          "Saper formalizzare il concetto di ordinamento di una sequenza",
+          "Simulare l'algoritmo di ordinamento per selezione, per inserimento e a bolle",
+          "Calcolare il numero di confronti e di scambi degli algoritmi di ordinamento basati su confronti e scambi",
+          "Comprendere i criteri di scelta di un algoritmo rispetto ad altri",
+          "Astrarre il modello di semplici problemi di natura quantitativa e descrivere algoritmicamente il procedimento di soluzione",
+          "Simulare l'esecuzione di un programma",
+        ],
       },
       {
-        argomento: "",
-        arg_breve: "",
-        area_tematica: "Architettura dei computer (AC)",
+        argomento: "Interagire col sistema operativo tramite la shell",
+        arg_breve: "Sistemi Operativi",
+        area_tematica: "Sistemi operativi (SO)",
+        rationale: `Le Indicazioni nazionali richiedono la conoscenza delle funzionalità dei sistemi operativi più comuni
+        e si è scelto di orientare lo studio al sistema operativo GNU/Linux in quanto esso è il sistema operativo
+        più eseguito\\footnote{Si veda la voce \\url{https://en.wikipedia.org/wiki/Usage_share_of_operating_systems}{Usage share of operating systems} di Wikipedia.}
+        e documentato fin nei minimi particolari ed è software libero. Nel primo bienno si è limitata l'analisi delle funzionalità al
+        concetto di \\textit{file system}, processo e di \\textit{shell}. Le interfacce grafiche per la gestione dei file sono ben note agli studenti e quindi si è scelto di lavorare sul linguaggio \\texttt{bash} e sui filtri più comuni.`,
+        risultati: `Gli studenti sanno condurre operazioni complesse impossibili da effettuare con le interfacce grafiche.`,
+        strumenti: `Si è optato per un laboratorio avente PC operanti con la distribuzione FUSS\\footnote{Si veda il \\url{https://fuss.bz.it/}{progetto FUSS}}.`,
+        abilita: [
+          "Creare, rinominare, spostare e cancellare un file",
+          "Organizzare i file nelle directory",
+          "Invocare un programma",
+          "Elencare file e directory",
+          "Creare una pipe anonima tra due o più utility",
+          "Usare comandi per la data, l'estrazione della testa e della coda di un file di testo, l'ordinamento delle linee, l'estrazione di campi",
+        ],
+      },
+      {
+        argomento: "Documenti elettronici per il web",
+        arg_breve: "Web",
+        area_tematica: "Elaborazione digitale dei documenti (DE)",
+        rationale: `Viene introdotto in anticipo rispetto alle Indicazioni nazionali per avere poi il tempo nel secondo biennio di introdurre la programmazione del web.`,
+        risultati: ``,
+        strumenti: ``,
+        abilita: [
+          "Riconoscere un linguaggio basato sui marcatori",
+          "Usare i tag HTML per creare un documento valido",
+          "Usare i tag semantici per strutturare il contenuto",
+          "Saper realizzare un semplice sito web statico curando solo il contenuto",
+          "Collegare un foglio di stile ad una pagina web",
+          "Modificare lo stile del testo",
+          "Modificare l'aspetto delle scatole",
+          "Saper realizzare un semplice sito web statico curando anche la presentazione",
+        ],
+      },
+    ],
+  },
+  {
+    periodo: "Secondo biennio",
+    argomenti: [
+      {
+        argomento: "Fogli elettronici",
+        arg_breve: "Fogli elettronici",
+        area_tematica: "Elaborazione digitale dei documenti (DE)",
         rationale: ``,
         risultati: ``,
         strumenti: ``,
-        abilita: ["", "", "", "", ""],
+        abilita: [
+          "Usare le operazioni di filtraggio, riduzione e mappa nel foglio di calcolo",
+          "Usare il foglio di calcolo per modellizzare e risolvere le problematiche d'interesse per il corso di studi",
+          "Usare il risolutore per problemi di scelta",
+          "Impostare problemi a variabili intere con il risolutore",
+          "Descrivere l'ordine delle operazioni nell'aggiornamento di un foglio di calcolo",
+        ],
       },
       {
-        argomento: "",
-        arg_breve: "",
-        area_tematica: "Architettura dei computer (AC)",
-        rationale: ``,
+        argomento: "Paradigmi di programmazione",
+        arg_breve: "Paradigmi",
+        area_tematica: "Algoritmi e linguaggi di programmazione (AL)",
+        rationale: `Nell'incapacità dell'autore di decodificare le richieste
+di \\textit{implementazione di un linguaggio di programmazione,
+metodologie di programmazione, sintassi di un linguaggio
+orientato agli oggetti}, si è scelto di presentare il paradigma
+della programmazione strutturata (con funzioni definite nell'ambito 
+di una funzione) in JavaScript, quello logico con esempi in Prolog,
+e quello funzionale in JavaScript, realizzando funzioni pure di ordine superiore.`,
         risultati: ``,
         strumenti: ``,
-        abilita: ["", "", "", "", ""],
+        abilita: [
+          "Saper realizzare algoritmi usando le funzioni e  i costrutti di sequenza, selezione e iterazione (paradigma strutturato)",
+          "Riconoscere la ricorsione come caratteristica sintattica",
+          "Riconoscere se il processo generato da una procedura sintatticamente ricorsiva è lineare o ricorsivo",
+          "Progettare semplici basi di fatti e regole con paradigma logico",
+          "Saper realizzare semplici algoritmi mediante funzioni sintatticamente ricorsive",
+          "Saper attraversare semplici strutture dati definite ricorsivamente",
+        ],
       },
       {
-        argomento: "",
-        arg_breve: "",
-        area_tematica: "Architettura dei computer (AC)",
-        rationale: ``,
-        risultati: ``,
-        strumenti: ``,
-        abilita: ["", "", "", "", ""],
-      },
-      {
-        argomento: "",
-        arg_breve: "",
-        area_tematica: "Architettura dei computer (AC)",
-        rationale: ``,
-        risultati: ``,
-        strumenti: ``,
-        abilita: ["", "", "", "", ""],
-      },
-      {
-        argomento: "",
-        arg_breve: "",
-        area_tematica: "Architettura dei computer (AC)",
+        argomento: "Documenti elettronici per il web",
+        arg_breve: "Web",
+        area_tematica: "Algoritmi e linguaggi di programmazione (AL)",
         rationale: ``,
         risultati: ``,
         strumenti: ``,
@@ -140,68 +199,105 @@ const context: Tratto[] = [
       },
     ],
   },
+  {
+    periodo: "Quinto anno",
+    argomenti: [
+      {
+        argomento: "",
+        arg_breve: "",
+        area_tematica: "Reti di computer (RC)",
+        rationale: ``,
+        risultati: ``,
+        strumenti: ``,
+        abilita: ["", "", "", "", ""],
+      },
+      {
+        argomento: "",
+        arg_breve: "",
+        area_tematica: "Struttura di Internet e servizi (IS)",
+        rationale: ``,
+        risultati: ``,
+        strumenti: ``,
+        abilita: ["", "", "", "", ""],
+      },
+      {
+        argomento: "Funzioni di ordine superiore",
+        arg_breve: "Funzioni di ordine superiore",
+        area_tematica: "Computazione, calcolo numerico e simulazione (CS)",
+        rationale: ``,
+        risultati: ``,
+        strumenti: ``,
+        abilita: [
+          "Definire funzioni che hanno come argomento una funzione di variabile reale e un valore reale e che restituiscono un valore reale",
+          "Definire funzioni che hanno come argomento una funzione di variabile reale e che restituiscono una funzione di variabile reale",
+          "Descrivere gli algoritmi in termini di applicazione e composizione di funzioni",
+          "Manipolare gli array usando le tecniche: filter, map, reduce.",
+        ],
+      },
+      {
+        argomento: "Radici di funzioni non lineari",
+        arg_breve: "Radici",
+        area_tematica: "Computazione, calcolo numerico e simulazione (CS)",
+        rationale: ``,
+        risultati: ``,
+        strumenti: ``,
+        abilita: [
+          "Saper applicare il metodo di Erone di Alessandria per il calcolo delle radici quadrate",
+          "Riconoscere l'esistenza di una radice di una funzione dato un intervallo",
+          "Saper effettuare la ricerca numerica di una radice tramite il metodo di bisezione",
+          "Realizzare in un linguaggio di programmazione e al foglio di calcolo il metodo di bisezione",
+          "Saper effettuare la ricerca numerica di una radice tramite il metodo delle tangenti (di Newton)",
+          "Realizzare in un linguaggio di programmazione e al foglio di calcolo il metodo delle tangenti (di Newton)",
+          "Saper riconoscere l'equivalenza di due metodi iterativi",
+        ],
+      },
+      {
+        argomento: "Ottimizzazione 1-dimensionale",
+        arg_breve: "Ottimizzazione",
+        area_tematica: "Computazione, calcolo numerico e simulazione (CS)",
+        rationale: ``,
+        risultati: ``,
+        strumenti: ``,
+        abilita: [
+          "Riconoscere l'esistenza di un massimo e di un minimo",
+          "Metodo di Newton per l'ottimizzazione",
+          "Approssimare il punto di minimo locale usando il metodo di Newton per l'ottimizzazione",
+          "Realizzare un un linguaggio di programmazione e al foglio di calcolo il metodo di Newton per l'ottimizzazione",
+          "Realizzare un un linguaggio di programmazione e al foglio di calcolo il metodo della sezione aurea",
+        ],
+      },
+    ],
+  },
 ];
 
-console.log(JSON.stringify(context, null, 2));
+//console.log(JSON.stringify(context, null, 2));
+let tex = "";
+context.forEach((ciclo: Ciclo) => {
+  tex += `\n\\subsection{${ciclo.periodo}}\n\\label{sec:${ciclo.periodo
+    .toLowerCase()
+    .replace(" ", "-")}}\n\n`;
 
+  ciclo.argomenti.forEach((arg: Argomento) => {
+    tex += `\\subsubsection[${arg.arg_breve}]{${arg.argomento}}\n`;
+    tex += `\\label{sec:${arg.arg_breve.toLowerCase().replace(" ", "-")}}\n\n`;
+
+    tex += `\\noindent\\textbf{Area tematica}: ${arg.area_tematica}\n\n`;
+    tex += `%\\noindent\\textbf{Abilità}:
+    
+\\begin{enumerate}\n`;
+    arg.abilita.forEach((abilita: string) => {
+      tex += `  \\item ${abilita}\n`;
+    });
+    tex += `\\end{enumerate}\n\n`;
+
+    tex += `${arg.rationale}\n\n`;
+    tex += `${arg.strumenti}\n\n`;
+    tex += `${arg.risultati}\n\n`;
+  });
+});
+
+console.log(tex);
 /*
-\subsection[Problemi e algoritmi]{Problemi, modelli, soluzioni e algoritmi}
-
-Per introdurre gli studenti alla programmazione vengono prima presentati
-e formalizzati i problemi.
-Si sono scelte le seguenti abilità:
-
-\begin{enumerate}
-  \item
-    Saper formalizzare un problema di ricerca
-  \item
-    Simulare l'esecuzione dell'algoritmo di ricerca lineare
-  \item
-    Simulare l'esecuzione dell'algoritmo di ricerca binaria
-  \item
-    Saper formalizzare il concetto di ordinamento di una sequenza
-  \item
-    Simulare l'algoritmo di ordinamento per selezione, per inserimento e a bolle
-  \item
-    Calcolare il numero di confronti e di scambi degli algoritmi di
-    ordinamento basati su confronti e scambi
-  \item
-    Comprendere i criteri di scelta di un algoritmo rispetto ad altri
-  \item
-    Astrarre il modello di semplici problemi di natura quantitativa e
-    descrivere algoritmicamente il procedimento di soluzione
-  \item
-    Simulare l'esecuzione di un programma
-\end{enumerate}
-  
-Anche in questo caso si è fatto ricorso a `Computer Science Unplugged'.
-
-\subsection[Sistemi Operativi]{Interagire col sistema operativo tramite la shell}
-
-Per introdurre il sistema operativo si è scelto di saper interagire con il suo
-livello più esterno sviluppando le seguenti abilità:
-
-\begin{enumerate}
-  \def\labelenumi{\arabic{enumi}.}
-  \item
-    Creare, rinominare, spostare e cancellare un file
-  \item
-    Organizzare i file nelle directory
-  \item
-    Invocare un programma
-  \item
-    Elencare file e directory
-  \item
-    Creare una pipe anonima tra due o più utility
-  \item
-    Usare comandi per la data, l'estrazione della testa e della coda
-    di un file di testo, l'ordinamento delle linee, l'estrazione di campi
-\end{enumerate}
-
-Si è fatto ricorso ad una connessione `ssh` ad un server GNU/Linux.
-
-\section{Secondo biennio e quinto anno}
-
 \subsection[Web]{Documenti elettronici per il web}
 
 Nel secondo biennio si è cercato di sviluppare, gradualmente, l'astrazione
@@ -216,19 +312,6 @@ introdurre i concetti di reattività ma anche di funzione di
 ordine superiore in modo intuitivo. Si sono stabiliti i seguenti
 traguardi:
 
-\begin{enumerate}
-  \item
-    Usare le operazioni di filtraggio, riduzione e mappa nel foglio di
-    calcolo
-  \item
-    Usare il foglio di calcolo per modellizzare e risolvere le
-    problematiche d'interesse per il corso di studi
-  \item
-    Usare il risolutore per problemi di scelta
-  \item
-    Impostare problemi a variabili intere con il risolutore
-\end{enumerate}
-
 \subsection[Paradigmi]{Paradigmi di programmazione}
 
 Nell'incapacità dell'autore di decodificare le richieste
@@ -240,15 +323,9 @@ di una funzione) in JavaScript, quello logico con esempi in Prolog,
 e quello funzionale in JavaScript, realizzando funzioni pure espresse
 senza l'uso dei costrutti \texttt{while}, \texttt{do-while} e texttt{for}.
 
-\begin{enumerate}
-  \item\
-    Saper realizzare algoritmi usando le funzioni e 
-    i costrutti di sequenza, selezione e iterazione (paradigma strutturato)
-  \item
-    Progettare semplici basi di fatti e regole con paradigma logico
-  \item
-    Saper realizzare semplici algoritmi mediante funzioni sintatticamente ricorsive
-\end{enumerate}
+"Saper realizzare algoritmi usando le funzioni e  i costrutti di sequenza, selezione e iterazione (paradigma strutturato)",
+"Progettare semplici basi di fatti e regole con paradigma logico",
+"Saper realizzare semplici algoritmi mediante funzioni sintatticamente ricorsive"
 
 \subsection[Grafi]{Problemi su grafi e combinatorici}
 
@@ -266,61 +343,9 @@ senza l'uso dei costrutti \texttt{while}, \texttt{do-while} e texttt{for}.
   \item Rappresentare sul foglio di calcolo il problema dello zaino
 \end{enumerate}
 
-\subsection[Funzioni di ordine superiore]{Funzioni di ordine superiore}
 
-\begin{enumerate}
-  \item
-    Definire funzioni che hanno come argomento una funzione di variabile
-    reale e un valore reale e che restituiscono un valore reale
-  \item
-    Definire funzioni che hanno come argomento una funzione di variabile
-    reale e che restituiscono una funzione di variabile reale
-  \item
-    Descrivere gli algoritmi in termini di applicazione e composizione di funzioni
-  \item
-    Manipolare gli array usando le tecniche: filter, map, reduce.
-\end{enumerate}
 
-\subsection[Radici]{Radici di funzioni}
-
-\begin{enumerate}
-  \item Saper applicare il metodo di Erone di Alessandria per il calcolo delle radici quadrate
-  \item
-    Riconoscere l'esistenza di una radice di una funzione dato un intervallo
-  \item
-    Saper effettuare la ricerca numerica di una radice tramite il metodo
-    di bisezione
-  \item
-    Realizzare in un linguaggio di programmazione e al foglio di calcolo
-    il metodo di bisezione
-    \item
-    Saper effettuare la ricerca numerica di una radice tramite il metodo
-    delle tangenti (di Newton)
-  \item
-    Realizzare in un linguaggio di programmazione e al foglio di calcolo
-    il metodo delle tangenti (di Newton)
-  \item
-    Saper riconoscere l'equivalenza di due metodi iterativi
-\end{enumerate}
-
-\subsection[Ottimizzazione]{Ottimizzazione nel caso di una variabile reale}
-
-\begin{enumerate}
-  \item
-  Riconoscere l'esistenza di un massimo e di un minimo
-  \item
-    Metodo di Newton per l'ottimizzazione
-  \item
-    Approssimare il punto di minimo locale usando il metodo di Newton
-  per l'ottimizzazione
-  \item
-    Realizzare un un linguaggio di programmazione e al foglio di calcolo
-  il metodo di Newton per l'ottimizzazione
-  \item
-    Realizzare un un linguaggio di programmazione e al foglio di calcolo
-  il metodo della sezione aurea
-\end{enumerate}
-
+\end{enumerate}"
 
 
 {
